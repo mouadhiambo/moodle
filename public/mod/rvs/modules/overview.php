10bc9ff24bda6ea555c1934264b642c1946f8b0a
Edgar Temba <mouadhiambo@gmail.com>
2025-10-09 09:44:46 +0300

    Show AI test button for admins in overview module
    Moved the AI test button to always display for administrators, regardless of AI configuration status. This improves accessibility for admin debugging and testing.

--- public/mod/rvs/modules/overview.php
+++ public/mod/rvs/modules/overview.php
@@ -35,14 +35,18 @@ if (!\mod_rvs\ai\generator::is_ai_configured()) {
         $configurl = new moodle_url('/admin/settings.php', array('section' => 'modsettingrvs'));
         $message .= html_writer::link($configurl, get_string('configurenow', 'mod_rvs'), 
             array('class' => 'btn btn-warning btn-sm'));
-        
-        // Add test button for debugging
-        $testurl = new moodle_url('/mod/rvs/test_ai.php', array('id' => $cm->id));
-        $message .= ' ' . html_writer::link($testurl, get_string('aitest', 'mod_rvs'), 
-            array('class' => 'btn btn-info btn-sm'));
     }
     
     echo $OUTPUT->notification($message, \core\output\notification::NOTIFY_WARNING);
+}
+
+// Always show test button for administrators
+if (has_capability('moodle/site:config', context_system::instance())) {
+    $testurl = new moodle_url('/mod/rvs/test_ai.php', array('id' => $cm->id));
+    echo html_writer::start_div('mb-3');
+    echo html_writer::link($testurl, get_string('aitest', 'mod_rvs'), 
+        array('class' => 'btn btn-info btn-sm'));
+    echo html_writer::end_div();
 }
 
 // Display content sources.
